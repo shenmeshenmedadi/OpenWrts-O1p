ed577635e03e9b6083a55b412c3fb1b5ede48583
shenmeshenmedadi <83005647+shenmeshenmedadi@users.noreply.github.com>
2024-04-18 21:41:46 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -4,6 +4,10 @@
 # 修改默认IP为192.168.10.1
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate 
 
+#kenzok8
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+
 # Hello World
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
